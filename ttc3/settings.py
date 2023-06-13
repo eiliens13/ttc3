c53d9d6fccf394687b39a0d3a792d3f9766fa897
Eiliens <daily.p.tito@gmail.com>
2023-06-13 05:58:57 -0400

    postgresql con psycopg2

--- ttc3/settings.py
+++ ttc3/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "ttc3.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": BASE_DIR / "TawaTaxi",
+        "USER": "postgres",
+        "PASSWORD": "superUserb4e+",
+        "HOST": "127.0.0.1",
+        "DATAVASE_PORT": "5432",
     }
 }
 
