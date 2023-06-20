03da3a5955d98cbc0b50726c1e15b127c9e059b7
Eiliens <daily.p.tito@gmail.com>
2023-06-20 17:01:52 -0400

    models update descripcion

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -11,8 +11,12 @@ class Clientes(models.Model):
 class Circuitos(models.Model):
     nombre = models.CharField(max_length=30)
     destinos_turisticos = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=1000)
+    descripcion = models.CharField(max_length=900, null=True, blank=True)
     precio = models.IntegerField()
+    
+
+    def __str__(self):
+        return "El nombre es %s, el destino turistico es %s, el precio es %s, la descripcion es %s" %(self.nombre, self.destinos_turisticos, self.descripcion, self.precio)
 
 class Pedidos(models.Model):
     numero = models.IntegerField()
