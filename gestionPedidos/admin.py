24e75bdc6a2c5668dc229fd3472e78d85a022329
Eiliens <daily.p.tito@gmail.com>
2023-06-24 06:31:06 -0400

    mejorando los modelos

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from gestionPedidos.models import Viajeros, Circuitos, Pedidos
 
 # Register your models here.
+admin.site.register(Viajeros)
+admin.site.register(Circuitos)
+admin.site.register(Pedidos)
--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 # Create your models here.
 
-class Clientes(models.Model):
+class Viajeros(models.Model):
     nombre = models.CharField(max_length=30)
     direccion = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.CharField(max_length=8)
 
 class Circuitos(models.Model):
-    nombre = models.CharField(max_length=30)
+    lugar = models.CharField(max_length=30)
     destinos_turisticos = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=900, null=True, blank=True)
     precio = models.IntegerField()
     
 
     def __str__(self):
-        return "El nombre es %s, el destino turistico es %s, la descripcion es %s, el precio es %s" %(self.nombre, self.destinos_turisticos, self.descripcion, self.precio)
+        return "Lugar: %s, Destino Turístico: %s, Descripción: %s, Precio: %s" %(self.lugar, self.destinos_turisticos, self.descripcion, self.precio)
 
 
 class Pedidos(models.Model):
