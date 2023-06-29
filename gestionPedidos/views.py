3f013acdc7781e5bc7a9ccc07fa3a951bea1895a
Eiliens <daily.p.tito@gmail.com>
2023-06-29 17:37:50 -0400

    restringiendo cant de caract

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -12,8 +12,11 @@ def buscar(request):
     if request.GET["cir"]:
         # mensaje = "Circuito encontrado: %r" %request.GET["cir"]
         circuito = request.GET["cir"]
-        circuitos = Circuito.objects.filter(lugar__icontains=circuito)
-        return render(request, "resultados_circuitos.html", {"circuitos":circuitos, "query":circuito})
+        if len(circuito)>20:
+            mensaje = "Texto de busqueda demasaido largo"
+        else:
+            circuitos = Circuito.objects.filter(lugar__icontains=circuito)
+            return render(request, "resultados_circuitos.html", {"circuitos":circuitos, "query":circuito})
     else:
         mensaje = "Debes introducir un destino para mostrarte nuestras ofertas 😉 "
     return HttpResponse(mensaje)
