62f39e2efd05f122135fd80e190f85fb863d48d3
eiliens13 <eiliens.tawa.tito@gmail.com>
2024-11-07 09:34:02 -0400

    Añadiendo postgres

--- ttc3/settings.py
+++ ttc3/settings.py
@@ -77,16 +77,17 @@ WSGI_APPLICATION = "ttc3.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": "tawataxi",
-#         "USER": "postgres",
-#         "PASSWORD": "superUserb4e+",
-#         "HOST": "127.0.0.1",
-#         "DATABASE_PORT": "5432",
-#     }
-# }
+DATABASES = {
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "superUserb4e+",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
 
 
 # Password validation
